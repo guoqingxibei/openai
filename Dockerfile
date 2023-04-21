bde6305b3393ec337525e521f9bfdefd09ebd6e4
guoqingj <guoqingj@vmware.com>
2023-04-21 15:40:35 +0800

    Use systemd to manage openai program

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine
 
 WORKDIR /app
 # 需要先本地编译，手动 GOOS=linux GOARCH=amd64 go build -o openai
-COPY openaiBin .
+COPY openai .
 
 RUN apk add tzdata && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
     && echo "Asia/Shanghai" > /etc/timezone \
@@ -10,4 +10,4 @@ RUN apk add tzdata && cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
 
 EXPOSE "$PORT"
 
-CMD ["./openaiBin"]
+CMD ["./openai"]
--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,13 @@
 #!/bin/bash -e
 cd ~/openai
-echo "Fetching Go code..."
+
+echo "Fetching openai code..."
 git fetch && git reset --hard origin/main
-echo "Building Go program..."
-go build -o openaiBin
 
-# Stop the running Go program
-echo "Stopping Go program..."
-pkill -f openaiBin || true
+echo "Building openai..."
+go build -o openai
+chown -R openai:openai ../openai
 
-# Wait for the program to stop
-sleep 1
-
-# Start the Go program
-echo "Starting Go program..."
-GO_ENV=prod nohup ./openaiBin > openai.log 2>&1 &
-
-# Check if the program started successfully
-if [ $? -eq 0 ]; then
-  echo "Go program restarted successfully"
-  tail -f log/data.log
-else
-  echo "Failed to restart Go program"
-fi
+echo "Restarting openai..."
+systemctl restart openai
+echo "Restarted openai successfully"
