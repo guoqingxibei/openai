90a4dd89cd422fc0037549fbc815b9b2aeafadce
guoqingj <guoqingj@vmware.com>
2023-05-24 23:31:30 +0800

    Filter printable chars before xml.Unmarshal

--- internal/handler/gateway.go
+++ internal/handler/gateway.go
@@ -8,6 +8,8 @@ import (
 	"openai/internal/constant"
 	"openai/internal/service/wechat"
 	"runtime/debug"
+	"strings"
+	"unicode"
 )
 
 var (
@@ -23,9 +25,10 @@ type ChatRound struct {
 // 微信服务器在五秒内收不到响应会断掉连接，并且重新发起请求，总共重试三次
 func Talk(writer http.ResponseWriter, request *http.Request) {
 	bs, _ := io.ReadAll(request.Body)
+	bs = []byte(filterPrintableChars(string(bs)))
 	inMsg, err := wechat.NewInMsg(bs)
 	if err != nil {
-		log.Printf("xml.Unmarshal error is %v, input is %s", err, string(bs))
+		log.Printf("xml.Unmarshal error is [%v], input is [%s]", err, string(bs))
 	}
 
 	// unhandled exception
@@ -64,6 +67,15 @@ func Talk(writer http.ResponseWriter, request *http.Request) {
 		log.Printf("未实现的消息类型: %s\n", inMsg.MsgType)
 		echoWechatTextMsg(writer, inMsg, "目前还只支持文本和语音消息哦~")
 	}
+}
+
+func filterPrintableChars(origin string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsPrint(r) {
+			return r
+		}
+		return -1
+	}, origin)
 }
 
 func echoWeChat(w http.ResponseWriter, data []byte) {
