7cb9d61c58411d87b501a9becda3530f1ae5c148
guoqingj <guoqingj@vmware.com>
2023-06-19 12:27:10 +0800

    Only 10 free quota every day

--- internal/logic/quota.go
+++ internal/logic/quota.go
@@ -11,10 +11,10 @@ import (
 )
 
 const boundaryTimestamp = 1681315200 // Thu Apr 13 2023 00:00:00 GMT+0800 (China Standard Time)
-const oldChatQuota = 20
+const oldChatQuota = 10
 
 var quotaMap = map[string]int{
-	constant.Chat:  20,
+	constant.Chat:  10,
 	constant.Image: 1,
 }
 
