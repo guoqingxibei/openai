e6da8c130a23cac030c34c1ddca3083ad25ac1ca
guoqingj <guoqingj@vmware.com>
2023-07-24 13:55:41 +0800

    Update deploy_brother.sh

--- deploy_brother.sh
+++ deploy_brother.sh
@@ -5,12 +5,13 @@ BRANCH=$ENV
 SERVICE_NAME=$ENV
 
 HK=47.56.184.46
-JUMP=52.29.29.173
+VM=10.221.14.56
 
 ssh root@$HK "cd /root/${SERVICE_NAME}/ && git fetch && git reset --hard origin/${BRANCH}"
-ssh guoqingj@$JUMP "./build_brother.sh ${ENV}"
+ssh guoqingj@$VM "./build_brother.sh ${ENV}"
 ssh root@$HK "rm -f /root/${SERVICE_NAME}/${SERVICE_NAME}"
 cd /tmp
-/Users/guoqingj/src/cloudmonitoring/tools/assistant.sh cp exp 192.168.9.99 /home/guoqingj/go/src/${SERVICE_NAME}/${SERVICE_NAME} && scp ${SERVICE_NAME} root@$HK:/root/${SERVICE_NAME}/
+scp guoqingj@$VM:~/go/src/${SERVICE_NAME}/${SERVICE_NAME} .
+scp ${SERVICE_NAME} root@$HK:/root/${SERVICE_NAME}/
 ssh root@$HK "chown -R ${SERVICE_NAME}:${SERVICE_NAME} /root/${SERVICE_NAME}/"
 ssh root@$HK "systemctl restart ${SERVICE_NAME}"
