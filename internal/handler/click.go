31a37254396c1e3a826479768ba9df686796b86c
guoqingj <guoqingj@vmware.com>
2023-09-16 01:29:26 +0800

    Update menu to support gpt4

--- internal/handler/click.go
+++ internal/handler/click.go
@@ -3,12 +3,17 @@ package handler
 import (
 	"log"
 	"net/http"
+	"openai/internal/constant"
 	"openai/internal/service/wechat"
 )
 
 func echoWechatOnClick(inMsg *wechat.Msg, writer http.ResponseWriter) {
 	log.Printf("%s clicked the button 「%s」", inMsg.FromUserName, inMsg.EventKey)
 	switch inMsg.EventKey {
+	case constant.GPT3:
+		fallthrough
+	case constant.GPT4:
+		switchGPTMode(inMsg.EventKey, inMsg, writer)
 	case clear:
 		clearHistory(inMsg, writer)
 	case help:
