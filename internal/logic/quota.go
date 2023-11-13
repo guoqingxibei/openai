d598088cf0d9bf4e8b69c4287a4c4168c7070577
guoqingj <guoqingj@vmware.com>
2023-11-13 14:03:08 +0800

    Improve the description

--- internal/logic/quota.go
+++ internal/logic/quota.go
@@ -21,6 +21,11 @@ func DecreaseBalance(userName string, mode string) (bool, string) {
 			gpt4BalanceTip := "【余额不足】抱歉，付费额度剩余%d次，不足以继续使用%s模式(每次绘画消耗次数%d)，" +
 				"<a href=\"%s\">点我充值次数</a>或者<a href=\"%s\">邀请好友获取次数</a>。" +
 				"\n\n%s在此模式下，每次对话仅消耗次数1。"
+			if mode == constant.GPT4 {
+				gpt4BalanceTip = "【余额不足】抱歉，付费额度剩余%d次，不足以继续使用%s模式(每次对话消耗次数%d)，" +
+					"<a href=\"%s\">点我充值次数</a>或者<a href=\"%s\">邀请好友获取次数</a>。" +
+					"\n\n%s在此模式下，每次对话仅消耗次数1。"
+			}
 			return false,
 				fmt.Sprintf(gpt4BalanceTip,
 					paidBalance,
@@ -40,7 +45,7 @@ func DecreaseBalance(userName string, mode string) (bool, string) {
 	if balance < timesPerQuestion {
 		paidBalance, _ := store.GetPaidBalance(userName)
 		if paidBalance < timesPerQuestion {
-			gpt3BalanceTip := "【余额不足】抱歉，你今天的免费额度(%d次)已用完，明天再来吧。费用昂贵，敬请谅解❤️\n\n" +
+			gpt3BalanceTip := "【余额不足】抱歉，你今天的免费额度(%d次)和付费额度已用完，明天再来吧。费用昂贵，敬请谅解❤️\n\n" +
 				"如果使用量很大，可以<a href=\"%s\">点我购买次数</a>或者<a href=\"%s\">邀请好友获取次数</a>。"
 			return false, fmt.Sprintf(gpt3BalanceTip,
 				GetQuota(userName),
