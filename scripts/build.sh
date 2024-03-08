548c5b462a2a05b595780eb51f88547f30b3eabc
guoqingj <guoqingj@vmware.com>
2024-03-08 16:41:31 +0800

    Use new build vm

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+
+FULL_SERVICE_NAME=$1
+BRANCH=$2
+
+REPO_DIR=~/go/src/${FULL_SERVICE_NAME}
+if [ ! -d ${REPO_DIR} ]; then
+  git clone https://github.com/guoqingXibei/openai.git ${REPO_DIR}
+fi
+cd ${REPO_DIR}
+git fetch && git reset --hard origin/${BRANCH}
+/usr/bin/go build -o ${FULL_SERVICE_NAME}
---
+++ scripts/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -xe
+
+# usage:
+#  scripts/deploy.sh openai staging
+#  scripts/deploy.sh openai
+#  scripts/deploy.sh brother
+
+SERVICE_NAME=$1 # openai or brother
+ENV=$2 # staging or empty
+
+if [ "$ENV" = "staging" ]; then
+  BRANCH="staging"
+  FULL_SERVICE_NAME="${SERVICE_NAME}-staging"
+else
+  BRANCH="main"
+  FULL_SERVICE_NAME="openai"
+fi
+
+VM=us-jumpbox
+HK=47.56.184.46
+
+ssh root@$HK "cd /root/${FULL_SERVICE_NAME}/ && git fetch && git reset --hard origin/${BRANCH}"
+ssh root@$HK "rm -f /root/${FULL_SERVICE_NAME}/${FULL_SERVICE_NAME}"
+
+ssh guoqingj@$VM "mkdir -p ~/openai/"
+scp scripts/build.sh guoqingj@$VM:~/openai/
+ssh guoqingj@$VM "chmod +x openai/build.sh && ./openai/build.sh ${FULL_SERVICE_NAME} ${BRANCH}"
+scp guoqingj@$VM:~/go/src/${FULL_SERVICE_NAME}/${FULL_SERVICE_NAME} root@$HK:/root/${FULL_SERVICE_NAME}/
+
+ssh root@$HK "chown -R ${FULL_SERVICE_NAME}:${FULL_SERVICE_NAME} /root/${FULL_SERVICE_NAME}/"
+ssh root@$HK "systemctl restart ${FULL_SERVICE_NAME}"
