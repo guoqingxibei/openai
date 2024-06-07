44d3eedb33a5a5b55669ebb7f727640ecbeccb84
Guoqing Jia <guoqingj@vmware.com>
2024-06-08 03:00:07 +0800

    Optimize deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,16 +16,20 @@ else
   FULL_SERVICE_NAME="${SERVICE_NAME}"
 fi
 
-VM=us-jumpbox
+# build
+IMAGE=golang:1.22
+WORKDIR=/app
+BIN_PATH=temp/bins/${FULL_SERVICE_NAME}
+PROXY_SERVER=http://10.221.14.35:3128
+OPTIONS="--rm -v .:${WORKDIR} -v ./temp/go-pkg-mod:/go/pkg/mod -w ${WORKDIR} -e https_proxy=${PROXY_SERVER}"
+docker run ${OPTIONS} ${IMAGE} go build -o ${BIN_PATH}
+
 HK=47.56.184.46
+ssh root@$HK "cd /root/${FULL_SERVICE_NAME}/ \
+&& git fetch && git reset --hard origin/${BRANCH} \
+&& rm -f /root/${FULL_SERVICE_NAME}/${FULL_SERVICE_NAME}"
 
-ssh root@$HK "cd /root/${FULL_SERVICE_NAME}/ && git fetch && git reset --hard origin/${BRANCH}"
-ssh root@$HK "rm -f /root/${FULL_SERVICE_NAME}/${FULL_SERVICE_NAME}"
+scp ${BIN_PATH} root@$HK:/root/${FULL_SERVICE_NAME}/
 
-ssh guoqingj@$VM "mkdir -p ~/openai/"
-scp scripts/build.sh guoqingj@$VM:~/openai/
-ssh guoqingj@$VM "chmod +x openai/build.sh && ./openai/build.sh ${FULL_SERVICE_NAME} ${BRANCH}"
-scp guoqingj@$VM:~/go/src/${FULL_SERVICE_NAME}/${FULL_SERVICE_NAME} root@$HK:/root/${FULL_SERVICE_NAME}/
-
-ssh root@$HK "chown -R ${FULL_SERVICE_NAME}:${FULL_SERVICE_NAME} /root/${FULL_SERVICE_NAME}/"
-ssh root@$HK "systemctl restart ${FULL_SERVICE_NAME}"
+ssh root@$HK "chown -R ${FULL_SERVICE_NAME}:${FULL_SERVICE_NAME} /root/${FULL_SERVICE_NAME}/ \
+&& systemctl restart ${FULL_SERVICE_NAME}"
