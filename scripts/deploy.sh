7a7e10003acedb8e455e3c974add106c362e3658
guoqingxibei <jia.guoqing@qq.com>
2024-09-12 15:56:35 +0800

    Remove temp dir from project

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,9 +19,9 @@ fi
 echo "[${FULL_SERVICE_NAME}] Building..."
 IMAGE=golang:1.22
 WORKDIR=/app
-BIN_PATH=temp/bins/${FULL_SERVICE_NAME}
+BIN_PATH=../openai-temp/bins/${FULL_SERVICE_NAME}
 PROXY_SERVER=http://10.221.14.35:3128
-OPTIONS="--rm -v .:${WORKDIR} -v ./temp/go-pkg-mod:/go/pkg/mod -w ${WORKDIR} -e https_proxy=${PROXY_SERVER}"
+OPTIONS="--rm -v .:${WORKDIR} -v ./../openai-temp/go-pkg-mod:/go/pkg/mod -w ${WORKDIR} -e https_proxy=${PROXY_SERVER}"
 docker run ${OPTIONS} ${IMAGE} go build -o ${BIN_PATH}
 
 echo "[${FULL_SERVICE_NAME}] Syncing..."
